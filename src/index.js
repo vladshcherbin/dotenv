7a37e74e7e04cd37c6e011d05ecab580e8c3d688
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-03-21 10:00:13 +0300

    Remove default dirname

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import path from 'path'
-import url from 'url'
 import dotenv from 'dotenv-safe'
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url))
-
-export default function loadEnv(directory = dirname) {
+export default function loadEnv(directory) {
   dotenv.config({
     path: `${directory}/.env`,
     sample: `${directory}/.env.example`
